9aecd0621885b4fb97514d185d0713ce8f15c612
Mirza Radoncic <34827462+mirzarad@users.noreply.github.com>
2019-12-19 19:38:47 -0500

    7:38M

--- src/main/java/com/maxminmajcdg/controller/PhaseController.java
+++ src/main/java/com/maxminmajcdg/controller/PhaseController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.maxminmajcdg.DemographicCategory;
 import com.maxminmajcdg.PartyCategory;
 import com.maxminmajcdg.dto.DemVotePair;
